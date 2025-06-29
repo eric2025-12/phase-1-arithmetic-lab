6b1be8221374da7dad0f8bbcaa1a4ad1a836ad75
eric2025.12 <ericmuigaikims.gmail.com>
2025-06-29 23:43:46 +0300

    lab5

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+// Multiply two numbers to get 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Modulo operation that returns 4
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+// Find the maximum number in a set
+const max = Math.max(5, 10, 15, 20);
+
